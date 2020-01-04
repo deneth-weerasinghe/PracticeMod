35524b7994c8bbc139048eea338cfbb3ee7b28be
Deneth Weerasinghe <deneth@hotmail.co.uk>
2020-01-04 01:29:21 +0000

    Removed implementation of ICapabilitySerializable in PlayerDispatcher

--- src/main/java/com/denethweerasinghe/practicemod/customclass/PlayerDispatcher.java
+++ src/main/java/com/denethweerasinghe/practicemod/customclass/PlayerDispatcher.java
@@ -4,33 +4,34 @@ import net.minecraft.nbt.CompoundNBT;
 import net.minecraft.util.Direction;
 import net.minecraftforge.common.capabilities.Capability;
 import net.minecraftforge.common.capabilities.CapabilityInject;
+import net.minecraftforge.common.capabilities.ICapabilityProvider;
 import net.minecraftforge.common.capabilities.ICapabilitySerializable;
 import net.minecraftforge.common.util.LazyOptional;
 
-import javax.annotation.Nonnull;
-
-public class PlayerDispatcher implements ICapabilitySerializable<CompoundNBT> {
+public class PlayerDispatcher implements ICapabilityProvider {
 
     @CapabilityInject(ICustomClass.class)
     public static final Capability<ICustomClass> PLAYER_COUNTER = null;
-    private LazyOptional<ICustomClass> instance = LazyOptional.of(PLAYER_COUNTER::getDefaultInstance);
 
-    @Nonnull
-    @Override
-    public <T> LazyOptional<T> getCapability(@Nonnull Capability<T> cap, Direction side) {
-        return cap == PLAYER_COUNTER ? instance.cast() : LazyOptional.empty();
-    }
+    private LazyOptional<ICustomClass> instance;
 
     @Override
-    public CompoundNBT serializeNBT() {
-//        CompoundNBT nbt = new CompoundNBT();
-//        return PLAYER_COUNTER.getDefaultInstance().saveNBTData(nbt);
-        return (CompoundNBT) PLAYER_COUNTER.getStorage().writeNBT(PLAYER_COUNTER, instance.orElseThrow(() -> new IllegalArgumentException("LazyOptional must not be empty!")), null);
+    public <T> LazyOptional<T> getCapability(Capability<T> cap, Direction side) {
+        if (cap != PLAYER_COUNTER)
+            return LazyOptional.empty();
+        if (this.instance == null){
+            this.instance = LazyOptional.of(PLAYER_COUNTER::getDefaultInstance);
+        }
+        return this.instance.cast();
     }
 
-    @Override
-    public void deserializeNBT(CompoundNBT nbt) {
-//        PLAYER_COUNTER.getDefaultInstance().loadNBTData(nbt);
-        PLAYER_COUNTER.getStorage().readNBT(PLAYER_COUNTER, instance.orElseThrow(() -> new IllegalArgumentException("LazyOptional must not be empty!")), null, nbt);
-    }
+//    @Override
+//    public CompoundNBT serializeNBT() {
+//        return (CompoundNBT) PLAYER_COUNTER.getStorage().writeNBT(PLAYER_COUNTER, this.getCapability(PLAYER_COUNTER, null).orElse(null), null);
+//    }
+//
+//    @Override
+//    public void deserializeNBT(CompoundNBT nbt) {
+//        PLAYER_COUNTER.getStorage().readNBT(PLAYER_COUNTER, this.getCapability(PLAYER_COUNTER, null).orElse(null), null, nbt);
+//    }
 }
