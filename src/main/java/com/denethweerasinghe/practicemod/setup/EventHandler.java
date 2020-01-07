520cc6977b4600f0619f7b5e9809eb1d46e995fb
Deneth Weerasinghe <deneth@hotmail.co.uk>
2020-01-07 20:52:06 +0000

    Gunpowder troubles

--- src/main/java/com/denethweerasinghe/practicemod/setup/EventHandler.java
+++ src/main/java/com/denethweerasinghe/practicemod/setup/EventHandler.java
@@ -4,6 +4,7 @@ import com.denethweerasinghe.practicemod.customclass.CustomClass;
 import com.denethweerasinghe.practicemod.customclass.ICustomClass;
 import net.minecraft.entity.player.PlayerEntity;
 import net.minecraft.item.Items;
+import net.minecraft.world.World;
 import net.minecraftforge.event.entity.player.PlayerInteractEvent;
 import net.minecraftforge.eventbus.api.SubscribeEvent;
 import net.minecraftforge.fml.common.Mod;
@@ -14,13 +15,14 @@ public class EventHandler {
     @SubscribeEvent
     public static void onRightClick(PlayerInteractEvent.RightClickItem event) {
             PlayerEntity player = event.getEntityPlayer();
-            PracticeMod.LOGGER.info("this **is** the server");
-            ICustomClass cap = CustomClass.getFromPlayer(player);
-            if (event.getItemStack().getItem() == Items.GUNPOWDER) {
-                cap.setCounter(cap.getCounter() + 10);
-                PracticeMod.LOGGER.info("new value is: " + cap.getCounter());
-            } else {
-                PracticeMod.LOGGER.info("gunpowder failure");
+            World world = event.getWorld();
+            if (!world.isRemote()) {
+                PracticeMod.LOGGER.info("this **is** the server");
+                ICustomClass cap = CustomClass.getFromPlayer(player);
+                if (event.getItemStack().getItem() == Items.GUNPOWDER) {
+                    cap.setCounter(cap.getCounter() + 10);
+                    PracticeMod.LOGGER.info("new value is: " + cap.getCounter());
+                }
             }
         }
 //    @SubscribeEvent
